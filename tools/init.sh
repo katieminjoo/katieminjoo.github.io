1cca64f561eff13a6424f7ce01ad3898d621d6d6
HaeHwan <0210hwan@gmail.com>
2020-02-18 20:51:57 +0900

    모든 띄어쓰기를 전부 없애보았따.

--- tools/init.sh
+++ tools/init.sh
@@ -5,15 +5,11 @@
 # https://github.com/cotes2020/jekyll-theme-chirpy
 # © 2019 Cotes Chung
 # Published under MIT License
-
 set -eu
-
 CATEGORIES=false
 TAGS=false
 LASTMOD=false
-
 WORK_DIR=$(dirname $(dirname $(realpath "$0")))
-
 check_status() {
   if [[ ! -z $(git status -s) ]]; then
     echo "Warning: Commit the changes of the repository first."
@@ -21,23 +17,17 @@ check_status() {
     exit 1
   fi
 }
-
-
 update_files() {
   python _scripts/py/init_all.py
   find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 }
-
-
 commit() {
   msg="Updated"
-
   if [[ ! -z $(git status categories -s) ]]; then
     git add categories/
     msg+=" the Categories"
     CATEGORIES=true
   fi
-
   if [[ ! -z $(git status tags -s) ]]; then
     git add tags/
     if [[ $CATEGORIES = true ]]; then
@@ -48,7 +38,6 @@ commit() {
     msg+=" Tags"
     TAGS=true
   fi
-
   if [[ ! -z $(git status _posts -s) ]]; then
     git add _posts/
     if [[ $CATEGORIES = true || $TAGS = true ]]; then
@@ -59,27 +48,18 @@ commit() {
     msg+=" Lastmod"
     LASTMOD=true
   fi
-
   if [[ $CATEGORIES = true || $TAGS = true || $LASTMOD = true ]]; then
     msg+=" for post(s)."
     git commit -m "[Automation] $msg"
   else
     msg="Nothing changed."
   fi
-
   echo $msg
 }
-
-
 main() {
-
   cd $WORK_DIR
-
   check_status
-
   update_files
-
   commit
 }
-
 main
